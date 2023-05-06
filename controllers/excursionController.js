233c6f53460ff26bf952d26271930a28a7547aa7
AlexanderChukhlantsev <alexander.chukhlantsev1@gmail.com>
2023-05-06 14:43:26 +0400

    доделал контролер экскурсий

--- controllers/excursionController.js
+++ controllers/excursionController.js
@@ -1,14 +1,14 @@
 import Excursion from "../models/Excursion.js";
-import Place from "../models/Place.js";
+import Excursion from "../models/Excursion.js";
 import { createError } from "../utils/error.js";
 
 export const createExcursion = async (req, res, next) => {
-	const placeId = req.params.placeid;
+	const excursionId = req.params.excursionid;
 	const newExcursion = new Excursion(req.body);
 	try {
 		const saveExcursion = await newExcursion.save();
 		try {
-			await Place.findByIdAndUpdate(placeId, {
+			await Excursion.findByIdAndUpdate(excursionId, {
 				$push : {excursion: saveExcursion._id},
 			});
 		} catch (err) {
@@ -19,3 +19,42 @@ export const createExcursion = async (req, res, next) => {
 		next(err);
 	}
 };
+export const updateExcursion = async (req, res, next) => {
+	try {
+		const updateExcursion = await Excursion.findByIdAndUpdate(
+			req.params.id, 
+			{$set: req.body}, 
+			{new:true});
+		res.status(200).json(updateExcursion);
+	}
+	catch (err) {
+		next(err);
+	}
+}
+export const deleteExcursion = async (req, res, next) => {
+	try {
+		await Excursion.findByIdAndDelete(req.params.id);
+		res.status(200).json("Место было удалено");
+	}
+	catch (err) {
+		next(err);
+	}
+}
+export const getExcursion = async (req, res, next) => {
+	try {
+		const excursion = await Excursion.findById(req.params.id);
+		res.status(200).json(excursion);
+	}
+	catch (err) {
+		next(err);
+	}
+}
+export const getAllExcursion = async (req, res, next) => {
+	try {
+		const excursions = await Excursion.find();
+		res.status(200).json(excursions);
+	}
+	catch (err) {
+		next(err);
+	}
+}
